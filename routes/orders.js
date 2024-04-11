a7360f50a2e5f1df04d8dcd16dcdf673835ae0f3
Pierluca Di Pasquale <dipasqualepierluca@gmail.com>
2024-04-11 16:38:14 +0200

    Added functions to find the customer who spent the most given a time interval and the total earnings given a time interval.

--- routes/orders.js
+++ routes/orders.js
@@ -4,11 +4,16 @@ const {
   createOrder,
   updateOrder,
   deleteOrder,
+  topCustomer,
+  totalRevenue
 } = require('../controllers/orders');
 
 const express = require('express');
 
 const router = express.Router();
+router.route('/top-customer').get(topCustomer);
+router.route('/total-revenue').get(totalRevenue);
+
 router.route('/').get(getOrders).post(createOrder);
 router.route('/:id').get(getOrder).put(updateOrder).delete(deleteOrder);
 
